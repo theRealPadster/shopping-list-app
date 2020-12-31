3af04e99bba93de0245a5519d07209918fad6e5b
Isaac Maier <padspambox@gmail.com>
2020-12-30 23:20:50 -0500

    ✨🚧 Load product data from JSON file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import logo from './logo.svg';
 import Category from './components/Category';
 import CategoryList from './components/CategoryList';
 import './App.css';
+import categories from './assets/products.json';
+// const csv = require('csvtojson');
+// import csv from 'csvtojson';
 
 function App() {
+
+  // TODO: figure out how to get csv loading working in browser
+  // (using JSON file currently, but that's not as easy to edit)
+  // csv().fromFile('./assets/products.csv')
+  // .then(data => {
+
+  // });
+
+  let Categories = categories.map((category, index) => (
+    <Category name={category.name} items={category.items} key={index}></Category>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,8 +27,7 @@ function App() {
       </header>
 
       <CategoryList>
-        <Category name="Produce" items={["Apples","Carrots","Pears"]}></Category>
-        <Category name="Desserts" items={["Ice cream","Pie","Custard"]}></Category>
+        {Categories}
       </CategoryList>
 
     </div>
